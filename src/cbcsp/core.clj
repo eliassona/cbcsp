366bbca5ac2bbf3ac964c43d0b03deda88195d75
Anders Eliasson <anders.eliasson@digitalroute.com>
2016-01-25 15:22:28 +0100

    impl

--- src/cbcsp/core.clj
+++ src/cbcsp/core.clj
@@ -13,12 +13,13 @@
 (defn subscribe [type]
   (let [m {:type type}]
     (fn [observable c]
-      (.subscribe 
-        observable
-        (reify Observer
-          (onNext [_ d] (put! c (assoc m :data d)))
-          (onError [_ e] (put! c (assoc m :error e)))
-          (onCompleted [_] ))))))
+      (let [s (atom nil)]
+        (.subscribe 
+          observable
+          (reify Observer
+            (onNext [_ d] (reset! s d))
+            (onError [_ e] (put! c (assoc m :error e)))
+            (onCompleted [_] (put! c (assoc m :data @s)))))))))
 
 (defn create! [bucket c]
   (let [s (subscribe :create)]
@@ -44,7 +45,7 @@
     (fn [id]
       (fn []
         (-> bucket
-          (.getAndLock id RawJsonDocument)
+          (.get id RawJsonDocument)
           (s c))
         c))))
 
@@ -77,8 +78,7 @@
   (fn [crud-fn]
     (go-loop 
       [retry 0]
-      (let [in-chan (crud-fn)
-            v (<! in-chan)]
+      (let [v (<! (crud-fn))]
         (if (contains? v :error)
           (if (and (< retry nr-of-retries) (retry? v))
             (do 
@@ -86,7 +86,42 @@
               (recur (inc retry)))
             (>! out-chan (assoc v :retry retry)))
           (>! out-chan v))))))
+
+
+
+(def crud-ops {:create create!, :read read, :update update!, :delete delete!})
+
+(defn crud-op-of [bucket crud-chan retry-chan nr-of-retries wait-btw-in-ms]
+  (into {} (map (fn [e] [(key e) (comp (retry-loop! retry-chan nr-of-retries wait-btw-in-ms) ((val e) bucket crud-chan))])) crud-ops))
   
+
+(defn consume [config crud-ops]
+  (let [{:keys [key-fn]} config
+        {:keys [create read update delete]} crud-ops]
+    (fn [data]
+      (let [k (key-fn data)]
+        (go-loop 
+          [retry 0]
+          (let [v (-> k read <!)]
+          )
+        )
+  ))))
+
+(defn consume-loop! [config]
+  (let [{:keys [in-chan bucket]} config
+        agg (consume config (crud-op-of bucket (chan)))]
+    (go-loop 
+      []
+      (when-let [data (<! in-chan)]
+        (agg data)
+        (recur)))
+    in-chan))
+
+(defn timeout-loop! []
+  )
+
+
+
 
 ;;------------------------------------test code-----------------------------------------------
 
@@ -119,3 +154,8 @@
   (let [c (chan)]
     ((retry-loop! c 15 500) ((read-and-lock! bucket (chan)) "0"))
     (<!! c)))
+
+
+(defn test-consume-loop []
+  (consume-loop! (concat {:in-chan (chan)} (crud-op-of bucket (chan)))))
+  
